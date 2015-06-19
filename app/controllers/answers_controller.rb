eb86268aab52487373c3043642700e00a56ef4d7
Hosanna Fuller <Miramonte23@gmail.com>
2015-06-19 10:22:54 -0700

    Update answers_controller.rb

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,7 @@
  class AnswersController < ApplicationController
 	def create
-    	@question = Question.find(params[:question_id])
-    	@answer = @question.answers.new(params_answer)
+    		@question = Question.find(params[:question_id])
+    		@answer = @question.answers.new(params_answer)
     	if @answer.save
       		redirect_to question_url(@question)
     	else
@@ -9,18 +9,18 @@
     	end
   	end
   	def vote
-    	@question = Question.find(params[:question_id])
-    	@answer = Answer.find(params[:id])
-    	@answer.votes += 1
-    	@answer.save
-    	redirect_to question_url(@question) 
+    		@question = Question.find(params[:question_id])
+    		@answer = Answer.find(params[:id])
+    		@answer.votes += 1
+    		@answer.save
+    		redirect_to question_url(@question) 
   	end
 private 
   def question_id
-  params.require(:question_id).permit(:question_id)
+  	params.require(:question_id).permit(:question_id)
   end
   def params_answer
-  params.require(:answer).permit(:description, :votes, :question_id)
+	 params.require(:answer).permit(:description, :votes, :question_id)
   end
 end
 
