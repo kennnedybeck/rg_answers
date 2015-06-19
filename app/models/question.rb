1b8758662c507a49d09dc328eeffb1570ed9669c
kennnedybeck <kennedybeck@gmail.com>
2015-06-19 12:22:33 -0700

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,12 @@
 class Question < ActiveRecord::Base
   has_many :answers
 
-  def print_answer
-    self.answers.each do |answer|
-       "hello" 
+  def top_answer
+    top_answer = self.answers.by_votes.first
+    if top_answer.present?
+      top_answer.description
+    else
+      "Be the first to answer this question!"
     end
   end
 end
